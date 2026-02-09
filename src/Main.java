fc60e684256aaa4b000f2470c480ca31912bf974
vOlive2.0 <141564336+vOlive-io@users.noreply.github.com>
2026-02-09 10:30:59 -0700

    YAY

--- src/Main.java
+++ src/Main.java
@@ -7,54 +7,63 @@ public class Main {
        Scanner scan = new Scanner(System.in);
        scan.close();
     }
-    public static String evaluatePostfix(String input) {
-        input = Arrays.toString(input.trim().split(" "));
-        if(input.length() <= 2) throw new IllegalArgumentException("Invalid");
+    public static int evaluatePostfix(String input) {
+        String[] inputs = input.split(" ");
         Stack ops = new Stack();
         String opsList = "+-*/^";
-        while(input.length() != 0) {
-            String ch = input.substring(input.length()-1);
-            if(opsList.indexOf(ch) == -1) {
-                ops.push(ch);
+        for(String i : inputs) {
+            if (opsList.indexOf(i) == -1) {
+                ops.push(i);
             } else {
                 int a = Integer.parseInt(ops.pop());
                 int b = Integer.parseInt(ops.pop());
-                ops.push(doMath(ch, a, b) + "");
+                ops.push(doMath(i, a, b) + "");
             }
-            input = input.substring(0, input.length()-1);
         }
         if(ops.size() > 1) throw new IllegalArgumentException("Invalid");
-        return ops.peek();
+        return Integer.parseInt(ops.peek());
     }
     public static String infixToPostfix(String input) {
-        input = Arrays.toString(input.trim().split(" "));
-        if(input.length() <= 2) throw new IllegalArgumentException("Invalid");
+        String[] inputs = input.split(" ");
         Stack ops = new Stack();
         String output = "";
         String opsList = "+-*/^";
-        while(input.length() != 0) {
-            String ch = input.substring(input.length()-1);
-            if(opsList.indexOf(ch) == -1) { //if letter
-               output += ch;
+        for(String i : inputs) {
+            if(opsList.indexOf(i) == -1) { //if letter
+               output += i;
+               output+=" ";
             } else { //if not a letter
-                if(ops.isEmpty()) {ops.push(ch);}
-                else if(checkHigher(ops.peek(), ch)) {
-                    output+=ch;
-                }
-                else {
-                    output+=ops.pop();
-                    ops.push(ch);
+                while(true) {
+                    if(ops.isEmpty()) {
+                        ops.push(i);
+                        break;
+                    }
+                    else if(checkHigher(ops.peek(), i)) {
+                        ops.push(i);
+                        break;
+                    }
+                    else {
+                        output+=ops.pop();
+                        output+=" ";
+                    }
                 }
             }
-            input = input.substring(0, input.length()-1);
+        }
+        while(!(ops.isEmpty())) {
+            output += ops.pop();
+            output += " ";
         }
         return output;
     }
     private static boolean checkHigher(String op, String ch) {
         String low = "+-";
         String high = "*/";
-        int chLv = Math.max(low.indexOf(ch), high.indexOf(ch));
-        int opLv = Math.max(low.indexOf(op), high.indexOf(op));
+        int chLv = 0; // *    2
+        int opLv = 0; // +    1
+        if(low.indexOf(ch) != -1) {chLv = 1;}
+        if(high.indexOf(ch) != -1) {chLv = 2;}
+        if(low.indexOf(op) != -1) {opLv = 1;}
+        if(high.indexOf(op) != -1) {opLv = 2;}
         if(chLv > opLv) {return true;}
         return false;
     }
--- src/MainTest.java
+++ src/MainTest.java
@@ -3,14 +3,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @org.junit.jupiter.api.Test
-    void main() {
-    }
-
-    @org.junit.jupiter.api.Test
     void evaluatePostfix() {
+        String easyProblem =      "1 2 +";
+        String mediumProblem =    "1 2 3 * +";
+        String hardProblem =      "1 2 3 * + 3 + 2 *";
+        String superHardProblem = "1 2 3 * + 3 + 2 * 5 4 * +";
+        assertEquals(3, Main.evaluatePostfix(easyProblem));
+        assertEquals(7, Main.evaluatePostfix(mediumProblem));
+        assertEquals(20, Main.evaluatePostfix(hardProblem));
+        assertEquals(40, Main.evaluatePostfix(superHardProblem));
     }
 
     @org.junit.jupiter.api.Test
     void infixToPostfix() {
+        String easyProblem =      "1 + 2";
+        String mediumProblem =    "1 + 2 * 3";
+        String hardProblem =      "A + B - C * D + E";
+        String superHardProblem = "A * B + A / B";
+        assertEquals("1 2 + ", Main.infixToPostfix(easyProblem));
+        assertEquals("1 2 3 * + ", Main.infixToPostfix(mediumProblem));
+        assertEquals("A B + C D * - E + ", Main.infixToPostfix(hardProblem));
+        assertEquals("A B * A B / + ", Main.infixToPostfix(superHardProblem));
     }
 }
